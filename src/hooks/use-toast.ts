5e9b310a6c98e80ae8605c94dd5a059ea3c3cfea
shavort3221 <shadowvortex9331@gmail.com>
2025-07-14 16:34:34 +0530

    Updated use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -174,7 +174,7 @@ function useToast() {
   React.useEffect(() => {
     listeners.push(setState)
     return () => {
-      const index = listeners.indexOf(setState)
+      const index = listeners.findIndex(listener => listener === setState)
       if (index > -1) {
         listeners.splice(index, 1)
       }
